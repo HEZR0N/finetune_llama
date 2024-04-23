63d2b53cb5d7a907aea3709cdc4316776107e24b
HEZR0N <99786488+HEZR0N@users.noreply.github.com>
2024-04-23 00:09:23 -0500

    Update finetune_mistral.py

--- finetune_mistral.py
+++ finetune_mistral.py
@@ -49,14 +49,13 @@ quant_config = BitsAndBytesConfig(
 model = AutoModelForCausalLM.from_pretrained(
     base_model,
     quantization_config=quant_config,
-    device_map={"": 0},
-    token=access_token
+    device_map={"": 0}
 )
 
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 
-tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True, token=access_token)
+tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 
@@ -121,15 +120,14 @@ load_model = AutoModelForCausalLM.from_pretrained(
     low_cpu_mem_usage=True,
     return_dict=True,
     torch_dtype=torch.float16,
-    device_map={"": 0},
-    token=access_token
+    device_map={"": 0}
 )
 
 new_test_model = PeftModel.from_pretrained(load_model, new_model)
 new_test_model = new_test_model.merge_and_unload()
 
 # Reload tokenizer to save it
-tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True, token=access_token)
+tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
 tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
